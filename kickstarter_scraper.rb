75573ec1d5a842f2814c44ea9342624f84f1b7c9
Martin Achtermann <mdacht777@gmail.com>
2020-07-06 00:09:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,16 @@ html = File.read('fixtures/kickstarter.html')
  
 kickstarter = Nokogiri::HTML(html)
 # binding.pry
-end
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+  end
 
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
